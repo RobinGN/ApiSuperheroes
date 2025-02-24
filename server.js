869d647164aa4177e91bd4135d0cd6989a10a669
Rositouwu74 <erickdavid74@hotmail.com>
2025-02-23 19:24:29 -0800

    villanos de DC

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.get("/api/villanos", (req, res) => {
   res.json(data);
 });
 
-// solo villanos de Marvel
+//villanos de Marvel
 app.get("/api/villanos/marvel", (req, res) => {
   const data = cargarDatos();
   res.json(data.base_de_datos.tablas.Marvel.datos);
 });
 
-// villano específico
+// villano por nombre en específico
 app.get("/api/villanos/marvel/:nombre", (req, res) => {
   const data = cargarDatos();
   const nombre = req.params.nombre;
@@ -38,6 +38,27 @@ app.get("/api/villanos/marvel/:nombre", (req, res) => {
     res.status(404).json({ error: "Villano no encontrado" });
   }
 });
+
+//Villanos DC
+app.get("/api/villanos/dc", (req, res) => {
+    const data = cargarDatos();
+    res.json(data.base_de_datos.tablas.DC.datos);
+  });
+  
+  //Villano por nombre en especifico
+  app.get("/api/villanos/dc/:nombre", (req, res) => {
+    const data = cargarDatos();
+    const nombre = req.params.nombre;
+    const villano = data.base_de_datos.tablas.DC.datos.find(
+      (v) => v.Nombre.toLowerCase() === nombre.toLowerCase()
+    );
+  
+    if (villano) {
+      res.json(villano);
+    } else {
+      res.status(404).json({ error: "Villano no encontrado" });
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(` Servidor corriendo en http://localhost:${PORT}`);
